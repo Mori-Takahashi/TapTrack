843b5f8563b2936a813305c6a4299c1925e344b8
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-12-03 16:01:44 +0100

    training componennt styling and add logic

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-training',
@@ -6,6 +6,35 @@ import { Component } from '@angular/core';
   templateUrl: './training.component.html',
   styleUrl: './training.component.scss'
 })
-export class TrainingComponent {
+export class TrainingComponent implements OnInit {
+
+  name: string | null = '';
+
+  ngOnInit() {
+    this.name = localStorage.getItem('name');
+  }
+
+
+  showOptions() {
+    let element = document.getElementById("options");
+    if (element) {
+      element.classList.add("show-options");
+      element.classList.add("slide-in-left");
+      element.classList.remove("d-none");
+    }
+  }
+
+  hideOption() {
+    let element = document.getElementById("options");
+    if (element) {
+      element.classList.remove("slide-in-left");
+      element.classList.add("slide-out-left")
+      setTimeout(() => {
+        element.classList.add("d-none");
+        element.classList.remove("show-options");
+        element.classList.remove("slide-out-left");
+      }, 500);
+    }
+  }
 
 }
