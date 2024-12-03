79f0c2bf116dbfbd54453ba187721f3b8a4356b0
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-12-03 15:15:49 +0100

    add training componennt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './home/login/login.component';
+import {TrainingComponent} from './training/training.component';
 
 export const routes: Routes = [
-
+  {path: '', redirectTo: "/login", pathMatch: "full"},
+  {path: 'login', component: LoginComponent},
+  {path: 'training', component: TrainingComponent}
 ];
