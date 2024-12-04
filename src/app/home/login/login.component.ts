4bfdddee2f24263b60e49a22ad88eb0fb842cd5b
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-12-04 16:29:27 +0100

    Add @angular/fire dependency and update CSS mapping
    This commit introduces the @angular/fire dependency to the project, as seen in the updated package-lock.json file. Additionally, it adjusts the CSS source map in basic.component.css.map to reflect changes made to the associated SCSS file. These updates improve integration with Firebase services and correct the stylesheet mapping for development purposes.

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
--- src/app/training/theorie/basic/basic.component.ts
+++ src/app/training/theorie/basic/basic.component.ts
@@ -1,17 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-basic',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './basic.component.html',
   styleUrl: './basic.component.scss'
 })
-export class BasicComponent {
+export class BasicComponent implements OnInit {
+  topics: { id: number; title: string; description: string }[] = [];
 
   constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.loadTopics();
+  }
+
   home() {
     this.router.navigate(['/training']);
+  }
+
+  openTopic(topicId: number) {
+    console.log(`Opening topic with ID: ${topicId}`);
+  }
+
+
+  /*TODO: Später über JSON einbinden!!*/
+  loadTopics() {
+    this.topics = [
+      {
+        id: 1,
+        title: 'Grundregeln des 10-Finger-Schreibens',
+        description: 'Lerne die wichtigsten Regeln für das effiziente Tippen mit dem 10-Finger-System. Von der richtigen Startposition bis zu Bewegungsabläufen.'
+      },
+      {
+        id: 2,
+        title: 'Tipps zur Handhaltung und Positionierung',
+        description: 'Erfahre, wie du deine Hände, Handgelenke und deinen Körper richtig positionierst, um bequem und ergonomisch zu tippen.'
+      },
+      {
+        id: 3,
+        title: 'Vorteile des 10-Finger-Systems',
+        description: 'Entdecke die Vorteile des 10-Finger-Schreibens, von schnellerem Tippen bis zu weniger Ermüdung bei langen Schreibsessions.'
+      }
+    ];
   }
 }
