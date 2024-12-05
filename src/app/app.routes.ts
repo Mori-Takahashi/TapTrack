01f2c7f5d6c2b5f624c955cb61de0e9a8cae6b71
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-12-05 15:18:37 +0100

    Add Taptrack route and update training link
    Introduced the Taptrack component route to the routing configuration to support new training module navigation. Also, updated the "Start" button on the training component page to link directly to the Taptrack route, enabling seamless user access to the new module.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {LoginComponent} from './home/login/login.component';
 import {TrainingComponent} from './training/training.component';
 import {BasicComponent} from './training/theorie/basic/basic.component';
+import {TaptrackComponent} from './training/praxis/taptrack/taptrack.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: "/login", pathMatch: "full"},
   {path: 'login', component: LoginComponent},
   {path: 'training', component: TrainingComponent},
-  {path: 'basic', component: BasicComponent}
+  {path: 'basic', component: BasicComponent},
+  {path: 'taptrack', component: TaptrackComponent}
 ];
