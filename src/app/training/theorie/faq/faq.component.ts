eac28f4629be8300804881f10979a596d2dbc907
Lyonel Berzen <lyonel.berzen@icloud.com>
2024-12-07 13:03:59 +0100

    styling and add cards for FAQ

--- src/app/training/theorie/faq/faq.component.ts
+++ src/app/training/theorie/faq/faq.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-faq',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './faq.component.scss'
 })
 export class FaqComponent {
+
+  constructor(private router: Router) {
+  }
+
+  home() {
+    this.router.navigate(['/training']);
+  }
+
+  clearLocalStorage() {
+    localStorage.removeItem("typingStats");
+    localStorage.removeItem("name");
+    console.log("Die Einträge 'typingStats' und 'name' wurden aus dem Local Storage gelöscht.");
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 1000);
+  }
 
 }
